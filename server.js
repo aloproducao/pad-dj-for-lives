17f2359012d4326b86901e4645446c039df3dcf3
aloproducao <43450424+aloproducao@users.noreply.github.com>
2023-08-22 01:49:34 -0300

    formatando codigo

--- server.js
+++ server.js
@@ -8,19 +8,21 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/getSounds', (req, res) => {
-    // Ajustar o caminho para a pasta sounds que está dentro de /public
-    const soundsDir = path.join(__dirname, 'public', 'sounds');
-    
-    fs.readdir(soundsDir, (err, files) => {
-        if (err) {
-            res.status(500).send({ error: 'Falha ao ler a pasta de sons.' });
-            return;
-        }
-        const audioFiles = files.filter(file => ['.mp3', '.wav'].includes(path.extname(file)));
-        res.send(audioFiles);
-    });
+  // Ajustar o caminho para a pasta sounds que está dentro de /public
+  const soundsDir = path.join(__dirname, 'public', 'sounds');
+
+  fs.readdir(soundsDir, (err, files) => {
+    if (err) {
+      res.status(500).send({ error: 'Falha ao ler a pasta de sons.' });
+      return;
+    }
+    const audioFiles = files.filter(file =>
+      ['.mp3', '.wav'].includes(path.extname(file)),
+    );
+    res.send(audioFiles);
+  });
 });
 
 app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000.');
+  console.log('Servidor rodando na porta 3000.');
 });
